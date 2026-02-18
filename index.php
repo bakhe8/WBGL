32bd8263e8d3d560ef5bf588d2a50e27718784b8
Bakheet Ahmed Al-Zahrani <bakheet@gmail.com>
2026-02-18 22:45:57 +0300

    fix(mobile): apply grid layout to record-actions to show release button

--- index.php
+++ index.php
@@ -403,7 +403,7 @@ $formattedSuppliers = array_map(function($s) {
     
     <!-- Main Application Styles -->
     <link rel="stylesheet" href="public/css/index-main.css">
-    <link rel="stylesheet" href="public/css/mobile.css?v=<?= time() + 4 ?>"> <!-- Mobile Retrofit (Cache Busted V5) -->
+    <link rel="stylesheet" href="public/css/mobile.css?v=<?= time() + 5 ?>"> <!-- Mobile Retrofit (Cache Busted V6) -->
     
     <!-- Mobile Logic -->
     <script src="public/js/mobile.js"></script>
